cedf4f97f0a359e9e2b34607d83f2fb9505cd164
Benigno <gomezbenigno333@gmail.com>
2016-09-11 18:37:36 -0400

    Tarea5

---
+++ app.js
@@ -0,0 +1,20 @@
+(function () {
+  console.log('Hola mundo');
+}());
+var variableArray = [];
+
+var variableString = 'carro';
+
+var variableNumerico = 69;
+
+variableArray.push('vaso');
+variableArray.push('lima'); 
+
+console.log(variableArray);
+
+var elObjeto = {};
+
+elObjeto.descripcion = 'edificio';
+elObjeto.cantidad = 1;
+
+console.log(elObjeto);
